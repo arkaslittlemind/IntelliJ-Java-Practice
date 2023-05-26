882ff96e8b081a0f84f3fd9f95d8e0b05e502c8b
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-26 22:26:57 +0530

    Create README.md

---
+++ LearnArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class LearnArrayList {
+    public static void main(String[] args) {
+        // ArrayList<String> studentsName = new ArrayList<>();
+        // studentsName.add("Arka");
+        // System.out.println(studentsName);
+
+
+
+    }
+}
