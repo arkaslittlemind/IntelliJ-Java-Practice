d6bee2428c4e8a88547aa1241d2cadb29bb08976
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-11 21:36:42 +0530

    master

--- ExceptionHandling/ExceptionDemo.java
+++ ExceptionHandling/ExceptionDemo.java
@@ -1,14 +1,21 @@
 package ExceptionHandling;
 
+
+import java.util.Scanner;
+
 public class ExceptionDemo {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         try {
             int c = a / b;
             System.out.println(c);
         } catch (ArithmeticException e) {
+            //System.out.println(e);
             System.out.println("Denominator should not be zero. TRY AGAIN!!");
         }
 
