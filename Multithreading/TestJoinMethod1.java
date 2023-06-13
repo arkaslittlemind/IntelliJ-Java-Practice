93bdab259c7a2f6348864ae39e37c117f72ca430
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-13 23:43:54 +0530

    master

--- Multithreading/TestJoinMethod1.java
+++ Multithreading/TestJoinMethod1.java
@@ -17,11 +17,19 @@ class TestJoinMethod1 extends Thread{
         TestJoinMethod1 t3 = new TestJoinMethod1();
         t1.start();
         try{
+            System.out.println("Thread 1 Starts ---");
             t1.join();
         }catch(Exception e){
             System.out.println(e);
         }
         t2.start();
+        try {
+            System.out.println("Thread 2 Starts ---");
+            t2.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Thread 3 Starts ---");
         t3.start();
     }
 } 
