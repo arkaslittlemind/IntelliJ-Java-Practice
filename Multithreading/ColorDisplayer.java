8305efcd1121941b3e8633af41e4b5badf3c288c
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-18 00:05:07 +0530

    master

--- Multithreading/ColorDisplayer.java
+++ Multithreading/ColorDisplayer.java
@@ -3,31 +3,31 @@ package Multithreading;
 import java.util.Random;
 
 public class ColorDisplayer implements Runnable {
-
-    private String[] colors = {"White","Blue","Black","Green","Red","Yellow"};
+    private String[] colors = {"white", "blue", "black", "green", "red", "yellow"};
 
     @Override
     public void run() {
         Random random = new Random();
-        String selectedColor;
+        int index;
 
         do {
-            int colorIndex = random.nextInt(colors.length);
-            selectedColor = colors[colorIndex];
-            System.out.println(selectedColor);
+            index = random.nextInt(colors.length);
+            String color = colors[index];
+            System.out.println(color);
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000); // Wait for 1 second between each color display
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        } while (!selectedColor.equals("red"));
-        System.out.println("Stop displaying the colors");
-    }
-    public static void main(String[] args) {
-        ColorDisplayer colorDisplayer = new ColorDisplayer();
-        Thread t = new Thread(colorDisplayer);
-        t.start();
+        } while (!colors[index].equals("red"));
+        System.out.println("Stop displaying colors");
     }
 
+    public static void main(String[] args) {
+        ColorDisplayer colorDisplay = new ColorDisplayer();
+        Thread thread = new Thread(colorDisplay);
+        thread.start();
+    }
 }
+
