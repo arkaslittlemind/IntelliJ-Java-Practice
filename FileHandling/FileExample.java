7f7ac4cdb0c4f639de7f75adb386cff21d8e42f8
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-17 23:27:04 +0530

    master

--- FileHandling/FileExample.java
+++ FileHandling/FileExample.java
@@ -1,19 +1,35 @@
 package FileHandling;
 
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileExample {
     public static void main(String[] args) {
+        String[] names = {"Arka", "Yash", "Debayan"};
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("F:/IntelliJ-Java-Practice/FileHandling/Test1.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("F://IntelliJ-Java-Practice//FileHandling//Test1.txt"));
             writer.write("Learn and practicing for final semester java exam");
+            writer.write("\nNew Sentence");
+
+            for(String name : names) {
+                writer.write("\n " + name);
+            }
             writer.close();
             System.out.println("Operation successful");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("F://IntelliJ-Java-Practice//FileHandling//Test1.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
