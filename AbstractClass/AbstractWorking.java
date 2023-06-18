fbdf5d973b7400db08884bb4c785f02b42eaec5e
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-18 16:24:16 +0530

    master

--- AbstractClass/AbstractWorking.java
+++ AbstractClass/AbstractWorking.java
@@ -3,12 +3,11 @@ package AbstractClass;
 public class AbstractWorking {
     public static void main(String[] args) {
         Rectangle r = new Rectangle();
+
         r.length = 10;
         r.breadth = 5;
 
-        Shape s = r;
-
-        System.out.println(s.area());
-        System.out.println(s.perimeter());
+        System.out.println(((Shape) r).area());
+        System.out.println(((Shape) r).perimeter());
     }
 }
